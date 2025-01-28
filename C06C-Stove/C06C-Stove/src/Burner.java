23c0b735b6823451d9a9e6159538ea364a3e9167
gdematta <gdematta@mines.edu>
2025-01-28 16:33:15 -0700

    Updated plus and minus button methods

--- C06C-Stove/C06C-Stove/src/Burner.java
+++ C06C-Stove/C06C-Stove/src/Burner.java
@@ -8,5 +8,48 @@ public class Burner {
 	}
 	
 	public Temperature myTemperature;
+	public Setting mySetting;
+	public int timer;
+	public final static int TIME_DURATION = 2;
+	
+	public Burner() {
+		this.myTemperature = Temperature.Cold;
+		this.mySetting = Setting.OFF;
+	}
+	
+	public void plusButton() {
+		switch(mySetting) {
+			case OFF:
+				mySetting = Setting.LOW;
+				break;
+			case LOW:
+				mySetting = Setting.MEDIUM;
+				break;
+			case MEDIUM:
+				mySetting = Setting.HIGH;
+				break;
+			case HIGH:
+				break;	
+		}
+		timer = TIME_DURATION;
+	}
+	
+	public void minusButton() {
+		switch(mySetting) {
+			case HIGH:
+				mySetting = Setting.MEDIUM;
+				break;
+			case MEDIUM:
+				mySetting = Setting.LOW;
+				break;
+			case LOW:
+				mySetting = Setting.OFF;
+				break;
+			case OFF:
+				break;	
+		}
+		timer = TIME_DURATION;
+	}
+	
 	
 }
